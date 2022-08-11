d5d7a606beb01656724edfba336e8845c399c015
mahati-basavaraju <110677094+mahati-basavaraju@users.noreply.github.com>
2022-08-12 00:38:15 +0530

    Update yosys_run.sh

--- yosys_run.sh
+++ yosys_run.sh
@@ -6,10 +6,11 @@ read_verilog iiitb_piso.v
 synth -top iiitb_piso
 
 # mapping to mycells.lib
-dfflibmap -liberty /home/lahari/Documents/Mahati/IIITB/Courses/Sem1/ASIC/Assignment_1_piso/iiitb_piso/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
-abc -liberty /home/lahari/Documents/Mahati/IIITB/Courses/Sem1/ASIC/Assignment_1_piso/lib/sky130_fd_sc_hd__tt_025C_1v80.lib -script +strash;scorr;ifraig;retime,{D};strash;dch,-f;map,-M,1,{D}
+dfflibmap -liberty /home/lahari/Documents/Mahati/IIITB/Courses/Sem1/ASIC/iiitb_piso/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+abc -liberty /home/lahari/Documents/Mahati/IIITB/Courses/Sem1/ASIC/iiitb_piso/lib/sky130_fd_sc_hd__tt_025C_1v80.lib -script +strash;scorr;ifraig;retime,{D};strash;dch,-f;map,-M,1,{D}
 clean
 flatten
 # write synthesized design
 write_verilog -noattr iiitb_piso_synth.v
 stat
+show
